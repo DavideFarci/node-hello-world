5afd3910c5ed37a1556864c291e175f77b8b30d0
DavideFarci <yank92.df@gmail.com>
2023-11-07 15:28:58 +0100

    add bonus, random phrase

--- server.js
+++ server.js
@@ -9,8 +9,22 @@ dotenv.config();
  */
 const port = +process.env.PORT || 3000;
 
+const welcome = [
+  `Ogni giorno è una nuova opportunità per cambiare la tua vita in meglio`,
+  `Le sfide sono solo opportunità travestite da difficoltà`,
+  `Il successo non è mai definitivo, e il fallimento non è mai fatale. Ciò che conta è il coraggio di continuare`,
+  `Le tue azioni parlano più forte delle tue parole. Sii la versione migliore di te stesso e ispira gli altri con il tuo esempio`,
+  `La felicità non è un destino da raggiungere, ma un modo di viaggiare. Trova gioia nelle piccole cose e vivi ogni momento con gratitudine`,
+];
+
+function htmlResponse(res) {
+  const randomPhrase = welcome[Math.floor(Math.random() * welcome.length)];
+  res.end(`<h1>${randomPhrase}</h1>`);
+}
+
 const server = http.createServer(function (req, res) {
-  res.end(`<h1>${process.env.SALUTO}</h1>`);
+  res.writeHead(200, { "Content-type": "text/html; charset=utf-8" });
+  htmlResponse(res);
 });
 
 server.listen(port, function () {
