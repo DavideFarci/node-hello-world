fa799e30d6bb6652a2b8eae2d57d22fc4f7fc314
DavideFarci <yank92.df@gmail.com>
2023-11-07 16:59:30 +0100

    add and remove comments

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Importo i dati
 const data = require("./data");
 const welcome = data.welcomePhrases;
 
@@ -10,14 +11,6 @@ dotenv.config();
  * @type {number}
  */
 const port = +process.env.PORT || 3000;
-
-// const welcome = [
-//   `Ogni giorno è una nuova opportunità per cambiare la tua vita in meglio`,
-//   `Le sfide sono solo opportunità travestite da difficoltà`,
-//   `Il successo non è mai definitivo, e il fallimento non è mai fatale. Ciò che conta è il coraggio di continuare`,
-//   `Le tue azioni parlano più forte delle tue parole. Sii la versione migliore di te stesso e ispira gli altri con il tuo esempio`,
-//   `La felicità non è un destino da raggiungere, ma un modo di viaggiare. Trova gioia nelle piccole cose e vivi ogni momento con gratitudine`,
-// ];
 
 function htmlResponse(res) {
   const randomPhrase = welcome[Math.floor(Math.random() * welcome.length)];
