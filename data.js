9a3f9ec20bea7445fb2043e73a7c6b400283703e
DavideFarci <yank92.df@gmail.com>
2023-11-07 16:54:55 +0100

    move data in other file

---
+++ data.js
@@ -0,0 +1,9 @@
+const welcomePhrases = [
+  `Ogni giorno è una nuova opportunità per cambiare la tua vita in meglio`,
+  `Le sfide sono solo opportunità travestite da difficoltà`,
+  `Il successo non è mai definitivo, e il fallimento non è mai fatale. Ciò che conta è il coraggio di continuare`,
+  `Le tue azioni parlano più forte delle tue parole. Sii la versione migliore di te stesso e ispira gli altri con il tuo esempio`,
+  `La felicità non è un destino da raggiungere, ma un modo di viaggiare. Trova gioia nelle piccole cose e vivi ogni momento con gratitudine`,
+];
+
+exports.welcomePhrases = welcomePhrases;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const http = require("http");
+const data = require("./data");
+const welcome = data.welcomePhrases;
 
+const http = require("http");
 // Imposto la libreria dotenv e la avvio
 const dotenv = require("dotenv");
 dotenv.config();
@@ -9,13 +11,13 @@ dotenv.config();
  */
 const port = +process.env.PORT || 3000;
 
-const welcome = [
-  `Ogni giorno è una nuova opportunità per cambiare la tua vita in meglio`,
-  `Le sfide sono solo opportunità travestite da difficoltà`,
-  `Il successo non è mai definitivo, e il fallimento non è mai fatale. Ciò che conta è il coraggio di continuare`,
-  `Le tue azioni parlano più forte delle tue parole. Sii la versione migliore di te stesso e ispira gli altri con il tuo esempio`,
-  `La felicità non è un destino da raggiungere, ma un modo di viaggiare. Trova gioia nelle piccole cose e vivi ogni momento con gratitudine`,
-];
+// const welcome = [
+//   `Ogni giorno è una nuova opportunità per cambiare la tua vita in meglio`,
+//   `Le sfide sono solo opportunità travestite da difficoltà`,
+//   `Il successo non è mai definitivo, e il fallimento non è mai fatale. Ciò che conta è il coraggio di continuare`,
+//   `Le tue azioni parlano più forte delle tue parole. Sii la versione migliore di te stesso e ispira gli altri con il tuo esempio`,
+//   `La felicità non è un destino da raggiungere, ma un modo di viaggiare. Trova gioia nelle piccole cose e vivi ogni momento con gratitudine`,
+// ];
 
 function htmlResponse(res) {
   const randomPhrase = welcome[Math.floor(Math.random() * welcome.length)];
