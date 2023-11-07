28f3c20359daf042b40aea619c61f914db780450
DavideFarci <yank92.df@gmail.com>
2023-11-07 14:37:07 +0100

    create server, add variable in env, print var

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const http = require("http");
+
+// Imposto la libreria dotenv e la avvio
+const dotenv = require("dotenv");
+dotenv.config();
+
+/**
+ * @type {number}
+ */
+const port = +process.env.PORT || 3000;
+
+const server = http.createServer(function (req, res) {
+  res.end(`<h1>${process.env.SALUTO}</h1>`);
+});
+
+server.listen(port, function () {
+  console.log("Server is running on http://localhost:" + port);
+});
